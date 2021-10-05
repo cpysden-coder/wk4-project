003e1071f20ee7921ec11547f2a36fd08f213502
cpysden-coder <chris_pysden@mac.com>
2021-10-05 10:16:45 -0700

    updates to trivia and team js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,55 @@
 console.log('hollow world');
+
+
+//Trivia Question function
+function getTrivia() {
+    triviaUrl = ("https://api.trivia.willfry.co.uk/questions?limit=5");
+    console.log(triviaUrl);
+  
+    //parse api endpoint for trivia
+    fetch(triviaUrl)
+    .then(function (response) {
+        console.log(response)
+        return response.json();
+      
+    })
+    .then(function(data) {
+        // console.log(data[0].category);
+        // var category = data[0].category;
+        // console.log(category);
+        // $('#appendHere').after(`<p id="cat">Category: ${category} </p>`)
+        var question = data[0].question;
+        console.log(question);
+        $('#trivia-question').text(`${question}?`)
+        // var correctAnswer = data[0].correctAnswer;
+        // console.log(correctAnswer);
+        // $('#ques').after(`<p id="corrAns">Correct Answer: ${correctAnswer} </p>`)
+  
+        
+    });
+    return
+  }
+  getTrivia()
+
+  var teamUrl = "https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=seattle%20seahawks"
+
+
+//team information
+fetch(teamUrl)
+.then(function (response) {
+    console.log(response)
+    return response.json();
+  
+})
+.then(function(data) {
+    console.log(data.teams[0].strTeamBadge);
+    var teamBadge = (`"${data.teams[0].strTeamBadge}"`);
+    console.log(teamBadge);
+    $('#team').after(`<img src=${teamBadge}></img>`)
+    // console.log(data.response[6].matchviewUrl);
+    // var scoreVideo = data.response[6].matchviewUrl
+    // $('#scores').after(`<img id="vid" src="${scoreVideo}"></img>`)
+    // console.log(data.response)
+
+
+})
